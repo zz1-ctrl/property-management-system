389750a3f3828ad295b0f3f5188e10889298337d
zz1-ctrl <3039537189@qq.com>
2026-03-23 19:18:47 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,35 +1,57 @@
-const { run } = require('runjs')
-const chalk = require('chalk')
-const config = require('../vue.config.js')
-const rawArgv = process.argv.slice(2)
-const args = rawArgv.join(' ')
+const Mock = require('mockjs')
+const { param2Obj } = require('./utils')
 
-if (process.env.npm_config_preview || rawArgv.includes('--preview')) {
-  const report = rawArgv.includes('--report')
+const user = require('./user')
+const table = require('./table')
 
-  run(`vue-cli-service build ${args}`)
+const mocks = [
+  ...user,
+  ...table
+]
 
-  const port = 9526
-  const publicPath = config.publicPath
+// for front mock
+// please use it cautiously, it will redefine XMLHttpRequest,
+// which will cause many of your third-party libraries to be invalidated(like progress event).
+function mockXHR() {
+  // mock patch
+  // https://github.com/nuysoft/Mock/issues/300
+  Mock.XHR.prototype.proxy_send = Mock.XHR.prototype.send
+  Mock.XHR.prototype.send = function() {
+    if (this.custom.xhr) {
+      this.custom.xhr.withCredentials = this.withCredentials || false
 
-  var connect = require('connect')
-  var serveStatic = require('serve-static')
-  const app = connect()
-
-  app.use(
-    publicPath,
-    serveStatic('./dist', {
-      index: ['index.html', '/']
-    })
-  )
-
-  app.listen(port, function () {
-    console.log(chalk.green(`> Preview at  http://localhost:${port}${publicPath}`))
-    if (report) {
-      console.log(chalk.green(`> Report at  http://localhost:${port}${publicPath}report.html`))
+      if (this.responseType) {
+        this.custom.xhr.responseType = this.responseType
+      }
     }
+    this.proxy_send(...arguments)
+  }
 
-  })
-} else {
-  run(`vue-cli-service build ${args}`)
+  function XHR2ExpressReqWrap(respond) {
+    return function(options) {
+      let result = null
+      if (respond instanceof Function) {
+        const { body, type, url } = options
+        // https://expressjs.com/en/4x/api.html#req
+        result = respond({
+          method: type,
+          body: JSON.parse(body),
+          query: param2Obj(url)
+        })
+      } else {
+        result = respond
+      }
+      return Mock.mock(result)
+    }
+  }
+
+  for (const i of mocks) {
+    Mock.mock(new RegExp(i.url), i.type || 'get', XHR2ExpressReqWrap(i.response))
+  }
 }
+
+module.exports = {
+  mocks,
+  mockXHR
+}
+
